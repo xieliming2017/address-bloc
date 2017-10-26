c0426ed6bbeec20f1e711810dcb3094fa401a932
xieliming2017 <xieliming2006@hotmail.com>
2017-10-25 23:02:13 -0400

    Assignment 34 models: completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -19,6 +19,7 @@ RSpec.describe AddressBook do
   end
 
   describe "#add_entry" do
+
     it "adds only one entry to the address book" do
       book = AddressBook.new
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
@@ -34,6 +35,36 @@ RSpec.describe AddressBook do
       expect(new_entry.name).to eq('Ada Lovelace')
       expect(new_entry.phone_number).to eq('010.012.1815')
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
+    end
+  end
+
+  describe "remove_entry" do
+    it "removes only one entry from the address book" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+      expect(book.entries.size).to eq(4)
+    end
+
+    it "removes the correct information to entries" do
+      book = AddressBook.new
+      book.add_entry('Alesssandro Nesta', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Andrea Pirlo', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Filippo Inzaghi', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Gennaro Gattuso', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Paolo Maldini', '010.012.1815', 'augusta.king@lovelace.com')
+      book.remove_entry('Andrea Pirlo', '010.012.1815', 'augusta.king@lovelace.com')
+
+      expect(book.entries.map{|e| [e.name, e.phone_number, e.email]}).to eq([['Alesssandro Nesta', '010.012.1815', 'augusta.king@lovelace.com'],
+                                 ['Filippo Inzaghi', '010.012.1815', 'augusta.king@lovelace.com'],
+                                 ['Gennaro Gattuso', '010.012.1815', 'augusta.king@lovelace.com'],
+                                 ['Paolo Maldini', '010.012.1815', 'augusta.king@lovelace.com']
+                                 ])
     end
   end
 end
