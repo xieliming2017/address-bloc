3874ebd3f0aabeb7bc81e6c0c92c9090b9bafeda
xieliming2017 <xieliming2006@hotmail.com>
2017-10-26 23:22:45 -0400

    Checkpoint 35 menu: completed

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,6 @@
-puts "Wlecom to AddressBloc!"
+require_relative 'controllers/menu_controller'
+
+menu = MenuController.new
+system "clear"
+puts "Wlecome to AddressBloc!"
+menu.main_menu
--- models/address_book.rb
+++ models/address_book.rb
@@ -20,4 +20,16 @@ class AddressBook
     entries.insert(index, Entry.new(name, phone_number, email))
   end
 
+  def remove_entry(name, phone_number, email)
+    index = 0
+    entries.each do |entry|
+      if name == entry.name
+        break
+      end
+      index += 1
+    end
+
+    entries.delete(entries[index])
+  end
+
 end
