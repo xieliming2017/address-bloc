9b01c67f167f5474930d96ce77b9d68960fe48cf
xieliming2017 <xieliming2006@hotmail.com>
2017-10-28 00:18:49 -0400

    Checkpoint 37 searching: Completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -36,11 +36,29 @@ class AddressBook
   def import_from_csv(file_name)
     csv_text = File.read(file_name)
     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
-  # #8
     csv.each do |row|
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
+  end
+
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+
+    while lower <= upper
+      mid = (lower + upper)/2
+      mid_name = entries[mid].name
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    return nil
   end
 
 end
